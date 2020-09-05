cac3668041f06928d2ad4aa5274a83c9a00069f6
orel aharonov <orel@invarial.com>
2020-09-05 17:23:00 +0300

    help

--- src/Server/utils.rs
+++ src/Server/utils.rs
@@ -1,6 +1,12 @@
-mod utils
+pub mod utils
 {
-    fn zfill(argument : i32, size : i32) -> String
+    use std::net::TcpStream;
+    /*
+    zero padding fill to a integer argument.
+    input: integer argument, size of padded string.
+    output: padded string.
+    */
+    pub fn zfill(argument : i32, size : i32) -> String
     {
         let mut padded = argument.to_string();
 
@@ -11,8 +17,19 @@ mod utils
         padded
     }
 
-    fn is_buffer_overflow(arguments_length : Vec<i32>, max_buffer_size : i32) -> bool
+    /*
+    check if there is an buffer overflow in TCP window (with a fixed size).
+    input: argument lengths vector, maximum buffer size.
+    output: buffer overflow statment.
+    */
+    pub fn is_buffer_overflow(arguments_length : Vec<i32>, max_buffer_size : i32) -> bool
     {
         arguments_length.iter().sum::<i32>() > max_buffer_size
     }
+
+    pub fn get_request_args(stream : TcpStream) -> Vec<String>
+    {
+        Vec::<String>::new()
+    }
 }
+
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
-mod utils;
+pub mod utils;
+use crate::server::utils::utils::get_request_args;
 use std::thread;
 use std::time::Duration;
 use std::net::TcpListener;
@@ -6,7 +7,7 @@ use std::net::TcpStream;
 use std::collections::VecDeque;
 use std::collections::HashSet;
 
-struct Server
+pub struct Server
 {
     _messages : VecDeque<String>,
     _users : HashSet<String>
@@ -23,8 +24,76 @@ impl Server
         }
     }
     
-    fn handle_client(stream : TcpStream)
+    pub fn start(mut self) ->  std::io::Result<()>
     {
+        let listener = TcpListener::bind("127.0.0.1:8826")?;
+
+        for stream in listener.incoming()
+        {
+            match stream {
+                Ok(stream) => {
+                    thread::spawn(move || {
+                        self.client_handler(stream);
+                    });
+                }
+                Err(e) => { /* connection failed */ }
+            }        
+        }
+
+        Ok(())
+    }
+
+    fn client_handler(&mut self, stream : TcpStream)
+    {
+        let mut status : (String, bool) = (String::new(), false);
+        let mut partner : String = String::new();
+
+        status = self.login(stream);
         
+        if status.1
+        {
+            loop
+            {
+                self.send_server_update(stream, status.0, partner);
+                partner = self.recv_client_update(stream, status.0);
+            }
+        }
+    }
+
+    fn login(&mut self, stream : TcpStream) -> (String, bool)
+    {
+        let mut args : Vec<String> = get_request_args(stream);
+
+        (String::new(), false)
+    }
+
+    fn recv_client_update(&mut self, stream : TcpStream, sender : String) -> String
+    {
+        String::new()
+    }
+
+    fn send_server_update(&mut self, stream : TcpStream, user : String, partner : String)
+    {
+
+    }
+
+    fn message_handler(&mut self)
+    {
+
+    }
+
+    fn create_chat_file(user1 : String, user2 : String) -> String
+    {
+        String::new()
+    }
+
+    fn update_chat_file(&mut self, fname : String, sender : String, data : String)
+    {
+
+    }
+
+    fn read_chat_file(fname : String) -> String
+    {
+        String::new()
     }
 }
